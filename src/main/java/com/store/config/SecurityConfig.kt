e1e4f1c992b06a2baf497b3d7751f44103eca2ac
Naresh Jain <nashjain@gmail.com>
2024-01-29 11:00:46 +0530

    Using templa,te to reduce duplication between SecurityConfig and DummySecurityConfig

--- src/main/java/com/store/config/SecurityConfig.kt
+++ src/main/java/com/store/config/SecurityConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter
 import org.springframework.security.web.SecurityFilterChain
@@ -16,10 +17,18 @@ open class SecurityConfig {
     open fun filterChain(http: HttpSecurity): SecurityFilterChain? {
         http
             .authorizeRequests { auth ->
-                auth.anyRequest().hasAuthority("SCOPE_email")
+                verifyAuthority(auth.anyRequest())
             }
-            .addFilterAfter(JwtAuthenticationFilter(), BearerTokenAuthenticationFilter::class.java)
-        http.oauth2ResourceServer { obj: OAuth2ResourceServerConfigurer<HttpSecurity?> -> obj.jwt() }
+        configureFilterChain(http)
         return http.build()
+    }
+
+    protected open fun configureFilterChain(http: HttpSecurity) {
+        http.addFilterAfter(JwtAuthenticationFilter(), BearerTokenAuthenticationFilter::class.java)
+        http.oauth2ResourceServer { obj: OAuth2ResourceServerConfigurer<HttpSecurity?> -> obj.jwt() }
+    }
+
+    protected open fun verifyAuthority(authorizedUrl: ExpressionUrlAuthorizationConfigurer<HttpSecurity>.AuthorizedUrl) {
+        authorizedUrl.hasAuthority("SCOPE_email")
     }
 }
--- src/test/java/com/store/config/DummySecurityConfig.kt
+++ src/test/java/com/store/config/DummySecurityConfig.kt
@@ -1,25 +1,21 @@
 package com.store.config
 
 import com.store.security.DummySecurityFilter
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter
 
 @Configuration
 @Profile("test")
-open class DummySecurityConfig {
-    @Bean
-    open fun dummyFilterChain(http: HttpSecurity): SecurityFilterChain? {
-        http
-            .authorizeRequests { auth ->
-                auth.anyRequest().permitAll()
-            }
-            .addFilterBefore(DummySecurityFilter(), AbstractPreAuthenticatedProcessingFilter::class.java)
-            .csrf().disable()
+open class DummySecurityConfig: SecurityConfig() {
+    override fun configureFilterChain(http: HttpSecurity) {
+        http.addFilterBefore(DummySecurityFilter(), AbstractPreAuthenticatedProcessingFilter::class.java)
+        http.csrf().disable()
+    }
 
-        return http.build()
+    override fun verifyAuthority(authorizedUrl: ExpressionUrlAuthorizationConfigurer<HttpSecurity>.AuthorizedUrl) {
+        authorizedUrl.permitAll()
     }
 }
